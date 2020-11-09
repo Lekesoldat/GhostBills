f864923430ea2ee32e1205b784f2de135f9fa79b
Magnus Holtet <magnusholtet@Magnuss-MacBook-Pro.local>
2020-11-10 00:16:53 +0100

    Remove catch in sign in

--- src/firebase/FirebaseAuthMethods.ts
+++ src/firebase/FirebaseAuthMethods.ts
@@ -16,12 +16,10 @@ export const signIn = (email: string, password: string) =>
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
-    .then((res) => console.log("You've been signed in!\n", res))
-    .catch((err) => console.error(err));
+    .then((res) => console.log("You've been signed in!\n", res));
 
 export const signOut = () =>
   firebase
     .auth()
     .signOut()
-    .then((res) => console.log("Successfully signed out!\n"))
-    .catch((err) => console.log(err));
+    .then((res) => console.log("Successfully signed out!\n"));
