a10600d112155747bfd6135f45f2ae53e4ee8ca0
Magnus Holtet <magnusholtet@Magnuss-MacBook-Pro.local>
2020-11-10 00:16:43 +0100

    Remove unused hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { signOut } from "../firebase/FirebaseAuthMethods";
-import useUser from "../hooks/useUser";
 
 const Home = () => {
-  const user = useUser();
-
   return (
     <>
       <button onClick={() => signOut()}>Sign out!</button>
