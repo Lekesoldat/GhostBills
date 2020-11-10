c57ce2127192e2a3a3b0818474b6949a18d0d569
Magnus Holtet <magnusholtet@Magnuss-MacBook-Pro.local>
2020-11-11 00:07:34 +0100

    Add global flex.

--- src/context/Authentication.tsx
+++ src/context/Authentication.tsx
@@ -1,8 +1,9 @@
+import { Flex } from "@chakra-ui/core";
 import firebase from "firebase";
 import React, { createContext, PropsWithChildren, useContext } from "react";
 import { Route, Switch } from "react-router";
+import Navigation from "../components/Navigation";
 import { ProtectedRoute } from "../components/PrivateRoute";
-import ThemeToggler from "../components/ThemeToggler";
 import useFirebaseAuthentication from "../hooks/useFirebaseAuthentication";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
@@ -20,12 +21,14 @@ const AuthProvider = ({ children }: PropsWithChildren<{}>) => {
 
   return (
     <AuthContext.Provider value={{ user }}>
-      <ThemeToggler />
-      <Switch>
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/sign-up" component={SignUp} />
-        <ProtectedRoute path="*">{children}</ProtectedRoute>
-      </Switch>
+      <Navigation />
+      <Flex justifyContent="center">
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
+          <ProtectedRoute path="*">{children}</ProtectedRoute>
+        </Switch>
+      </Flex>
     </AuthContext.Provider>
   );
 };
