0e30aa9c71f058f014ecf878280813c36d19d6da
Magnus Holtet <magnulho@gmail.com>
2020-11-12 18:17:58 +0100

    Update FirestoreMethods to aggregate totals.

--- src/firebase/FirestoreMethods.ts
+++ src/firebase/FirestoreMethods.ts
@@ -1,5 +1,6 @@
 import firebase from "firebase";
 import Subscription from "../typeDefs";
+import { calculateTotals } from "../utils";
 
 export const getSubscriptions = async (userId: string) => {
   const response = await firebase
@@ -10,14 +11,89 @@ export const getSubscriptions = async (userId: string) => {
   return response.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
 };
 
-export const createSubscription = (
+export const createSubscription = async (
   userId: string,
   newSubscription: Subscription
-) =>
-  firebase
-    .firestore()
-    .collection(`users/${userId}/subscriptions`)
-    .add(newSubscription);
+) => {
+  const user = firebase.firestore().collection("users").doc(userId);
+
+  const userFields = (await user.get()).data();
+
+  if (!userFields) {
+    throw "userFields not found!";
+  }
+
+  // Update user fields
+  for (const [key, value] of Object.entries(
+    calculateTotals(newSubscription.recurring, newSubscription.cost)
+  )) {
+    if (userFields[key]) {
+      userFields[key] += value;
+    } else {
+      userFields[key] = value;
+    }
+  }
+
+  await user.update(userFields);
+
+  // Add subscription
+  await user.collection("subscriptions").add(newSubscription);
+};
+
+export const updateSubscription = async (
+  userId: string,
+  docId: string,
+  updatedSubscription: Subscription
+) => {
+  const user = firebase.firestore().collection("users").doc(userId);
+
+  const userFields = (await user.get()).data();
+
+  if (!userFields) {
+    throw "userFields not found!";
+  }
+
+  // Update user fields
+  for (const [key, value] of Object.entries(
+    calculateTotals(updatedSubscription.recurring, updatedSubscription.cost)
+  )) {
+    if (userFields[key]) {
+      userFields[key] -= value;
+    }
+  }
+
+  await user.update(userFields);
+
+  await user.collection("subscriptions").doc(docId).update(updatedSubscription);
+};
+
+export const deleteSubscription = async (
+  userId: string,
+  docId: string,
+  deletedSubscription: Subscription
+) => {
+  const user = firebase.firestore().collection("users").doc(userId);
+
+  const userFields = (await user.get()).data();
+
+  if (!userFields) {
+    throw "userFields not found!";
+  }
+
+  // Update user fields
+  for (const [key, value] of Object.entries(
+    calculateTotals(deletedSubscription.recurring, deletedSubscription.cost)
+  )) {
+    if (userFields[key]) {
+      userFields[key] -= value;
+    }
+  }
+
+  await user.update(userFields);
+
+  // Delete subscription
+  await user.collection("subscriptions").doc(docId).delete();
+};
 
 export const getSubscription = (userId: string, docId: string) =>
   firebase
@@ -25,21 +101,3 @@ export const getSubscription = (userId: string, docId: string) =>
     .collection(`users/${userId}/subscriptions/${docId}`)
     .doc(docId)
     .get();
-
-export const updateSubscription = (
-  userId: string,
-  docId: string,
-  updatedSubscription: Subscription
-) =>
-  firebase
-    .firestore()
-    .collection(`users/${userId}/subscriptions/${docId}`)
-    .doc(docId)
-    .update(updatedSubscription);
-
-export const deleteSubscription = (userId: string, docId: string) =>
-  firebase
-    .firestore()
-    .collection(`users/${userId}/subscriptions`)
-    .doc(docId)
-    .delete();
