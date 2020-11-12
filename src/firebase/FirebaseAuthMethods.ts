9e69cc52fb52959bccee6a460781437785e44182
Magnus Holtet <magnulho@gmail.com>
2020-11-13 00:34:42 +0100

    Remove logging

--- src/firebase/FirebaseAuthMethods.ts
+++ src/firebase/FirebaseAuthMethods.ts
@@ -9,17 +9,10 @@ export const signUp = (username: string, email: string, password: string) =>
         .firestore()
         .collection("users")
         .doc(res.user?.uid)
-        .set({ username, email })
+        .set({ username, email, yearly: 0, monthly: 0, weekly: 0, daily: 0 })
     );
 
 export const signIn = (email: string, password: string) =>
-  firebase
-    .auth()
-    .signInWithEmailAndPassword(email, password)
-    .then((res) => console.log("You've been signed in!\n", res));
+  firebase.auth().signInWithEmailAndPassword(email, password);
 
-export const signOut = () =>
-  firebase
-    .auth()
-    .signOut()
-    .then((res) => console.log("Successfully signed out!\n"));
+export const signOut = () => firebase.auth().signOut();
