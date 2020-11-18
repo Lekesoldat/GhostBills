7fa16e15d94e05ff112ba53b7d5504d631459c24
Magnus Holtet <magnulho@gmail.com>
2020-11-18 22:22:18 +0100

    Add lodash

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const multiplicands = {
   yearly: 1,
   monthly: 12,
@@ -12,9 +14,9 @@ export const calculateTotals = (
   const yearlyPrice = price * multiplicands[recurring];
 
   return {
-    yearly: yearlyPrice,
-    monthly: yearlyPrice / 12,
-    weekly: yearlyPrice / 52,
-    daily: yearlyPrice / 365,
+    yearly: _.round(yearlyPrice, 2),
+    monthly: _.round(yearlyPrice / 12, 2),
+    weekly: _.round(yearlyPrice / 52, 2),
+    daily: _.round(yearlyPrice / 365, 2),
   };
 };
