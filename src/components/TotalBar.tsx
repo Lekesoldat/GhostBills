25727d46e901978ecb2695fcd67c5ab3b2bd71c6
Magnus Holtet <magnulho@gmail.com>
2020-11-21 17:48:14 +0100

    Add mobile support for totals

--- src/components/TotalBar.tsx
+++ src/components/TotalBar.tsx
@@ -35,17 +35,23 @@ const TotalBar = () => {
 
   return (
     <StatGroup
-      borderRadius="lg"
+      d="grid"
+      gridTemplateColumns={{
+        base: "1fr 1fr",
+        lg: "1fr 1fr 1fr 1fr",
+      }}
       borderWidth="1px"
+      borderRadius="lg"
+      justifyContent="space-between"
       p={5}
       m={5}
-      justifyContent="space-between"
     >
       <Stat
         d="flex"
         justifyContent="center"
-        borderRight="2px solid"
-        borderRightColor="green.300"
+        borderBottom={{ base: "2px solid pink", md: "none" }}
+        borderRight="2px solid pink"
+        p={2}
       >
         <StatLabel>Yearly</StatLabel>
         <StatNumber>{fields.yearly}</StatNumber>
@@ -55,26 +61,22 @@ const TotalBar = () => {
       <Stat
         d="flex"
         justifyContent="center"
-        borderRight="2px solid"
-        borderRightColor="green.300"
+        borderBottom={{ base: "2px solid pink", md: "none" }}
+        borderRight={{ lg: "2px solid pink" }}
+        p={2}
       >
         <StatLabel>Monthly</StatLabel>
         <StatNumber>{fields.monthly}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
       </Stat>
 
-      <Stat
-        d="flex"
-        justifyContent="center"
-        borderRight="2px solid"
-        borderRightColor="green.300"
-      >
+      <Stat d="flex" justifyContent="center" borderRight="2px solid pink" p={2}>
         <StatLabel>Weekly</StatLabel>
         <StatNumber>{fields.weekly}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
       </Stat>
 
-      <Stat d="flex" justifyContent="center">
+      <Stat d="flex" justifyContent="center" p={2}>
         <StatLabel>Daily</StatLabel>
         <StatNumber>{fields.daily}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
