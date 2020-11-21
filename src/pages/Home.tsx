5e78c2f9fb78490e2ee64abd706fedc07e10d226
Magnus Holtet <magnulho@gmail.com>
2020-11-21 14:17:31 +0100

    Add wrapper for content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import { Box } from "@chakra-ui/react";
 import React from "react";
 import SubscriptionList from "../components/SubscriptionList";
+import TotalBar from "../components/TotalBar";
+import useUser from "../hooks/useUser";
 const Home = () => {
+  const user = useUser();
   return (
-    <>
-      {/* <TotalBar /> */}
+    <Box borderRadius="lg" borderWidth="1px" width="80%">
+      <TotalBar />
       <SubscriptionList />
-    </>
+    </Box>
   );
 };
 
