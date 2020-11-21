1ef8344b90cb746d89cb7ebacb1178da26562016
Magnus Holtet <magnulho@gmail.com>
2020-11-21 14:32:01 +0100

    Add disclaimer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import SubscriptionList from "../components/SubscriptionList";
 import TotalBar from "../components/TotalBar";
@@ -7,6 +7,10 @@ const Home = () => {
   const user = useUser();
   return (
     <Box borderRadius="lg" borderWidth="1px" width="80%">
+      <Text textAlign="center">
+        ⚠ Note that the following data is mocked and that the app is not yet
+        fully functional ⚠
+      </Text>
       <TotalBar />
       <SubscriptionList />
     </Box>
