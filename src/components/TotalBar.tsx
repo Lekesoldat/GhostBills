43d5d75b5ee99ca43a023c2de92c0e6c18a43c53
Magnus Holtet <magnulho@gmail.com>
2020-11-21 14:17:25 +0100

    Update Totals

--- src/components/TotalBar.tsx
+++ src/components/TotalBar.tsx
@@ -13,14 +13,20 @@ import { SubscriptionTotals } from "../typeDefs";
 
 const TotalBar = () => {
   const user = useUser();
-  const [fields, setFields] = useState<SubscriptionTotals | null>();
+  const [fields, setFields] = useState<SubscriptionTotals | null>({
+    daily: 15.74,
+    monthly: 478.75,
+    weekly: 110.48,
+    yearly: 5745,
+  });
 
   useEffect(() => {
     const fetchUserFields = async () => {
       const totals = await getSubscriptionTotals(user.uid);
       setFields(totals);
     };
-    fetchUserFields();
+
+    // fetchUserFields();
   }, []);
 
   if (!fields) {
@@ -28,26 +34,47 @@ const TotalBar = () => {
   }
 
   return (
-    <StatGroup>
-      <Stat>
+    <StatGroup
+      borderRadius="lg"
+      borderWidth="1px"
+      p={5}
+      m={5}
+      justifyContent="space-between"
+    >
+      <Stat
+        d="flex"
+        justifyContent="center"
+        borderRight="2px solid"
+        borderRightColor="green.300"
+      >
         <StatLabel>Yearly</StatLabel>
         <StatNumber>{fields.yearly}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
       </Stat>
 
-      <Stat>
+      <Stat
+        d="flex"
+        justifyContent="center"
+        borderRight="2px solid"
+        borderRightColor="green.300"
+      >
         <StatLabel>Monthly</StatLabel>
         <StatNumber>{fields.monthly}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
       </Stat>
 
-      <Stat>
+      <Stat
+        d="flex"
+        justifyContent="center"
+        borderRight="2px solid"
+        borderRightColor="green.300"
+      >
         <StatLabel>Weekly</StatLabel>
         <StatNumber>{fields.weekly}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
       </Stat>
 
-      <Stat>
+      <Stat d="flex" justifyContent="center">
         <StatLabel>Daily</StatLabel>
         <StatNumber>{fields.daily}</StatNumber>
         <StatHelpText>NOK</StatHelpText>
