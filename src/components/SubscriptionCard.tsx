abdf9d0a96e811bb149b2ff3c8a366f57cafacb0
Magnus Holtet <magnulho@gmail.com>
2020-11-21 14:17:01 +0100

    Add subscription-cards

--- src/components/SubscriptionCard.tsx
+++ src/components/SubscriptionCard.tsx
@@ -1,5 +1,8 @@
+import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
+import useUser from "../hooks/useUser";
 import { Subscription } from "../typeDefs";
+import { Badges } from "../utils";
 
 interface SubscriptionData {
   data: Subscription;
@@ -8,14 +11,25 @@ interface SubscriptionData {
 const SubscriptionCard = ({
   data: { id, name, cost, recurring, type },
 }: SubscriptionData) => {
+  const user = useUser();
   return (
-    <div>
-      <div>ID: {id}</div>
-      <div>Name: {name}</div>
-      <div>Cost: {cost}</div>
-      <div>Recurring: {recurring}</div>
-      <div>Type: {type}</div>
-    </div>
+    <Flex
+      p={5}
+      m={5}
+      justifyContent="space-between"
+      borderRadius="lg"
+      borderWidth="1px"
+    >
+      <Flex direction="column">
+        <Box>{name}</Box>
+        {Badges[type]}
+      </Flex>
+
+      <Flex direction="column">
+        <Box textAlign="end">{cost},- </Box>
+        <Box color="gray.400">{recurring}</Box>
+      </Flex>
+    </Flex>
   );
 };
 
--- src/components/SubscriptionList.tsx
+++ src/components/SubscriptionList.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from "@chakra-ui/react";
+import { Box, CircularProgress } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import { getSubscriptions } from "../firebase/FirestoreMethods";
 import useUser from "../hooks/useUser";
@@ -28,17 +28,59 @@ const SubscriptionList = () => {
   const user = useUser();
   const [subscriptions, setSubscriptions] = useState<Subscription[] | null>([
     {
-      id: "RpcUQD3A1bw5ODxDNpMn",
-      type: "entertainment",
-      cost: 25,
+      id: "6vIrGNJ2ydVRDanQKZWw",
+      recurring: "yearly",
+      name: "Bear Pro",
+      type: "productivity",
+      cost: 149,
+    },
+    {
+      id: "HAf9o9CJ3L9pzrz4vy4o",
       recurring: "monthly",
+      type: "administrative",
+      name: "iCloud",
+      cost: 10,
+    },
+    {
+      id: "PKOMjQnE35qBh4jeby5B",
+      type: "administrative",
+      name: "1Password",
+      cost: 27,
+      recurring: "monthly",
+    },
+    {
+      id: "RpcUQD3A1bw5ODxDNpMn",
       name: "Spotify",
+      cost: 25,
+      type: "entertainment",
+      recurring: "monthly",
+    },
+    {
+      id: "Y4IqFxI121cVXvk6FPwJ",
+      type: "productivity",
+      cost: 29,
+      name: "TickTick",
+      recurring: "monthly",
+    },
+    {
+      id: "gNdaDMXEzsFRhk5aZRB4",
+      name: "Impulse",
+      cost: 299,
+      type: "training",
+      recurring: "monthly",
+    },
+    {
+      id: "kU6z1Jd0ATszp8eJzEU5",
+      cost: 580,
+      recurring: "yearly",
+      type: "entertainment",
+      name: "PlayStation Plus",
     },
     {
       id: "uaLVuIiBvzykGdDYCY0A",
-      type: "entertainment",
-      recurring: "monthly",
       cost: 28,
+      recurring: "monthly",
+      type: "entertainment",
       name: "Netflix",
     },
   ]);
@@ -56,16 +98,18 @@ const SubscriptionList = () => {
     // fetchSubscriptions();
   }, []);
 
+  console.log(subscriptions);
+
   if (subscriptions === null) {
     return <CircularProgress isIndeterminate />;
   }
 
   return (
-    <div>
+    <Box>
       {subscriptions.map((sub) => (
         <SubscriptionCard data={sub} />
       ))}
-    </div>
+    </Box>
   );
 };
 
